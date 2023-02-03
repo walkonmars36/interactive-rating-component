a0418f9e848eac20f170b71dc0dca45202062df5
Mark Lawson <mark@walkonmars.co.uk>
2023-02-03 12:53:07 +0000

    challenge complete

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 const form = document.getElementById("rating-form");
+const rating = document.getElementsByName("rating");
+const ratingNumber = document.querySelector(".card__rating-selection-number");
+const initialCardState = document.querySelector(".card__content-start");
+const finishCardState = document.querySelector(".card__content-finish");
+
+const displayFinishCardState = () => {
+  initialCardState.style.display = "none";
+  finishCardState.style.display = "flex";
+};
+
+const getRatingValue = () => {
+  for (let i = 0; i < rating.length; i++) {
+    if (rating[i].checked) {
+      ratingNumber.textContent = rating[i].value;
+      displayFinishCardState();
+    }
+  }
+};
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log(e.target);
+  getRatingValue();
 });
