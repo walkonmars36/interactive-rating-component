58b9b9e11ba5a30d3bb74d4d747f4f92ca75479f
Mark Lawson <mark@walkonmars.co.uk>
2023-03-29 22:34:16 +0100

    challenge finished - almost!

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 const form = document.getElementById("rating-form");
 const ratings = document.querySelectorAll('input[name="rating"]');
 const ratingNumber = document.querySelector(".card__rating-selection-number");
-const initialCardState = document.querySelector(".card__content-start");
-const finishCardState = document.querySelector(".card__content-finish");
+const initialCardState = document.querySelector(".card__start");
+const finishCardState = document.querySelector(".card__finish");
 const errorMessage = document.querySelector(".error-message");
+const submitBtn = document.querySelector("[type='submit']");
+const resetBtn = document.querySelector(".card__reset-btn");
+
+//////////// FUNCTIONS //////////////
 
 const displayFinishCardState = function () {
   initialCardState.style.display = "none";
   finishCardState.style.display = "flex";
+  resetBtn.style.visibility = "visible";
 };
 
 const getRatingValue = function () {
@@ -19,22 +24,45 @@ const getRatingValue = function () {
   });
   if (checkedRating) {
     ratingNumber.textContent = checkedRating;
-    displayFinishCardState();
     hideErrorMessage();
+    displayFinishCardState();
   } else {
     ratingNumber.textContent = "";
+    submitBtn.style.opacity = 0.3;
+    submitBtn.ariaDisabled = true;
+    submitBtn.disabled = true;
+    submitBtn.style.cursor = "not-allowed";
     displayErrorMessage("Please select a rating");
   }
 };
 
 const hideErrorMessage = function () {
+  // update aria hidden to true
   errorMessage.style.display = "none";
+  submitBtn.style.opacity = 1;
+  submitBtn.ariaDisabled = false;
+  submitBtn.disabled = false;
+  submitBtn.style.cursor = "pointer";
 };
 
 const displayErrorMessage = function (message) {
+  // update aria hidden to false
+
   errorMessage.textContent = message;
   errorMessage.style.display = "block";
 };
+
+const resetForm = function () {
+  ratings.forEach((rating) => {
+    rating.checked = false;
+  });
+
+  initialCardState.style.display = "flex";
+  finishCardState.style.display = "none";
+  resetBtn.style.visibility = "hidden";
+};
+
+//////////// EVENT LISTENERS //////////////
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -46,3 +74,5 @@ ratings.forEach((rating) => {
     hideErrorMessage();
   });
 });
+
+resetBtn.addEventListener("click", resetForm);
